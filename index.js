4fece197e504644a3b0783e5dca3a4c405176a5a
williameverettgeorge@icloud.com <williameverettgeorge@icloud.com>
2018-09-01 02:30:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function sayHiToGrandma(string) {
+  if (string ===string.toLowerCase()){
+    return "I can't hear you!"
+  } else if (string===string.toUpperCase()){
+    return "YES INDEED!"
+  } else if (string=== "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
